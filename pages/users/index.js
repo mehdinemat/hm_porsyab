dac9f900e2b241addd9256880cc612d771b1edda
mehdinemat <nematnejad.mm@gmail.com>
2025-05-19 12:05:33 -0700

    fix: remove useTranslatin duplicate import form users page

--- pages/users/index.js
+++ pages/users/index.js
@@ -9,7 +9,6 @@ import useSWR from "swr";
 import { NumberParam, useQueryParams, withDefault } from "use-query-params";
 import { useRouter } from "next/router";
 import { Box, Button, Grid, HStack, Input, InputGroup, InputRightElement, Stack, Text } from "@chakra-ui/react";
-import { useTranslation } from "react-i18next";
 import { BiSortAlt2 } from "react-icons/bi";
 import { IoSearch } from "react-icons/io5";
 
